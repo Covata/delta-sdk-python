0c80aaa374ffd6cec4a85fc3c5128327ea30f44d
Andrew Lum <andrew.lum@covata.com>
2017-03-09 15:44:09 +1100

    Removed unused import in example (#74)

--- examples/helloworld/helloworld.py
+++ examples/helloworld/helloworld.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from covata.delta import Client, ApiClient, FileSystemKeyStore
+from covata.delta import Client, FileSystemKeyStore
 
 
 def main():
